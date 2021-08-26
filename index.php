db49d509b6a6fe8a49ed4514defa22f83027569a
humza024 <humza.arshad024@gmai.com>
2021-08-26 06:34:03 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>GitHub Actions with Docker container</title>
+</head>
+<?php
+echo "PHP Code ok";
+?>
+<body>
+<h1>My name is humza Arshad khan</h1>
+<p>I am a DevOps Engineer</p>
+</body>
+</html>
